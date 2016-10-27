976eafe9e07a1a0ada2637396ced6413b84cdda1
Jazib Nasim <jazib.nasim@gmail.com>
2016-10-27 15:41:58 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+//Fahrenheit to Celsius
+var fahrenheit = 32
+var celsius = (fahrenheit-32) / 1.8;
+
+console.log("The temperature is " + celsius + "C");
+
+
+
+//Celsius to Fahrenheit
+var celsius = 0
+var fahrenheit = (celsius*1.8) + 32;
+
+console.log("The temperature is " + fahrenheit + "F");
